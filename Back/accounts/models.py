33d432d0b37568a9b6b76d9739071216ccf99726
JK <worudp12@gmail.com>
2022-05-17 15:32:38 +0900

    #1 Feat: User model 재정의

--- Back/accounts/models.py
+++ Back/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class User(AbstractUser):
+    age = models.IntegerField()
+    nickname = models.CharField(max_length=10)
+    region = models.CharField(max_length=10)
+    profile_image =  models.TextField(null=True)
+    search_keyword = ArrayField(models.CharField(max_length=20, null=True), size=5)
--- Back/movie_wiki_api/urls.py
+++ Back/movie_wiki_api/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('api/v1/accounts/', include('dj_rest_auth.urls')),
     
     # 회원가입 
-    path('api/v1/accounts/signup/', include('dj_rest_auth.registrations.urls')),
+    path('api/v1/accounts/signup/', include('dj_rest_auth.registration.urls')),
 ]
