ab39f4615639c6b7f4431455e1d108c4721f1c54
JK <worudp12@gmail.com>
2022-05-23 03:33:01 +0900

    #58 Feat: MovieDetail 오버뷰 구현

---
+++ final-pjt-front/movie-wiki/src/components/MovieDetailCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div
+      :style="{ backgroundImage: `url(${imageUrl})`, width, height }"
+      class="movie-detail-card"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MovieDetailCard",
+  props: {
+    image: {
+      type: String,
+    },
+    width: {
+      type: String,
+    },
+    height: {
+      type: String,
+    },
+  },
+  computed: {
+    imageUrl() {
+      const url = `https://image.tmdb.org/t/p/original${this.image}`;
+      return url;
+    },
+  },
+};
+</script>
+
+<style>
+.movie-detail-card {
+  border-radius: 15px;
+  background-color: #2f3136;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+</style>
--- final-pjt-front/movie-wiki/src/store/modules/movies.js
+++ final-pjt-front/movie-wiki/src/store/modules/movies.js
@@ -1,47 +1,52 @@
-// import drf from "@/api/drf";
-// import axios from "axios";
+import drf from "@/api/drf";
+import axios from "axios";
+import router from "@/router";
 
 export default {
   state: {
+    movieDetail: {},
     // searchInfos: null,
     // keyword: null,
   },
-  getters: {},
+  getters: {
+    // 영화 상세 정보
+    movieDetail: (state) => state.movieDetail,
+  },
   mutations: {
-    // SEARCH_INFO(state, data) {
-    //   // console.log(items);
-    //   // if (items.length !== 0) {
-    //   state.searchInfos = data[0];
-    //   state.keyword = data[1];
-    //   // console.log(data[0]);
-    //   // } else if (items === 0) {
-    //   //   state.searchInfos = items;
-    //   // }
-    // },
+    SET_MOVIE_DETAIL: (state, data) => (state.movieDetail = data),
   },
   actions: {
-    // searchInfo(context, keyword) {
-    //   const params = {
-    //     query: keyword,
-    //     type: "title",
-    //   };
-    //   // console.log(keyword);
-    //   axios({
-    //     url: drf.movie.movieSearch(),
-    //     method: "get",
-    //     params,
-    //   })
-    //     .then((response) => {
-    //       console.log(response.data);
-    //       const data = [response.data, keyword];
-    //       context.commit("SEARCH_INFO", data);
-    //       // commit("SEARCH_RECENT", query);
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
-    //       const data = [0, keyword];
-    //       context.commit("SEARCH_INFO", data);
-    //     });
-    // },
+    // 영화 상세정보 vuex에 저장하기
+    setMovieDetail({ commit }, data) {
+      commit("SET_MOVIE_DETAIL", data);
+    },
+
+    // 영화 상세정보 요청하기
+    fetchMovieDetail({ dispatch }, movieId) {
+      axios({
+        url: drf.movie.movieDetail(movieId),
+        method: "get",
+      })
+        .then((response) => {
+          dispatch("setMovieDetail", response.data);
+        })
+        .catch((error) => {
+          if (error.response.status === 404) {
+            router.push({ name: "NotFound" });
+          }
+        });
+    },
+
+    // 영화 좋아요
+    likeMovie({ getters, dispatch }, movieId) {
+      axios({
+        url: drf.movie.likeMovie(movieId),
+        method: "post",
+        headers: getters.authHeader,
+      }).then((response) => {
+        console.log(response);
+        dispatch("fetchMovieDetail", movieId);
+      });
+    },
   },
 };
