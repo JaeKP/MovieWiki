1beff3c6103a69c9f0dce35d4bda13b0f46e4f3b
yun <97648143+shrewslampe@users.noreply.github.com>
2022-05-23 16:10:53 +0900

    #55 Feat: 유저정보 에러

--- final-pjt-front/movie-wiki/src/store/modules/articles.js
+++ final-pjt-front/movie-wiki/src/store/modules/articles.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import drf from "@/api/drf";
 import router from "@/router";
-// import _ from "lodash";
+import _ from "lodash";
 
 export default {
   state: {
@@ -11,14 +11,17 @@ export default {
   getters: {
     articles: (state) => state.articles,
     article: (state) => state.article,
+    isAuthor: (state, getters) => {
+      return state.article.user?.username === getters.currentUser.username;
+    },
+    isArticle: (state) => !_.isEmpty(state.article),
   },
 
   mutations: {
     SET_ARTICLES: (state, articles) => (state.articles = articles),
     SET_ARTICLE: (state, article) => (state.article = article),
-    isAuthor: (state, getters) => {
-      return state.article.user?.username === getters.currentUser.username;
-    },
+    SET_ARTICLE_COMMENTS: (state, comments) =>
+      (state.article.comment = comments),
   },
   actions: {
     fetchArticles({ commit, getters }) {
@@ -62,6 +65,48 @@ export default {
             router.push({ name: "NotFound404" });
           }
         });
+    },
+    createComment({ commit, getters }, { articlePk, content }) {
+      /* 댓글 생성
+      POST: comments URL(댓글 입력 정보, token)
+        성공하면
+          응답으로 state.article의 comments 갱신
+        실패하면
+          에러 메시지 표시
+      */
+      const comment = { content };
+
+      axios({
+        url: drf.article.comments(articlePk),
+        method: "post",
+        data: comment,
+        headers: getters.authHeader,
+      })
+        .then((response) => {
+          commit("SET_ARTICLE_COMMENTS", response.data);
+        })
+        .catch((error) => console.error(error.response));
+    },
+    updateComment({ commit, getters }, { articlePk, commentPk, content }) {
+      /* 댓글 수정
+      PUT: comment URL(댓글 입력 정보, token)
+        성공하면
+          응답으로 state.article의 comments 갱신
+        실패하면
+          에러 메시지 표시
+      */
+      const comment = { content };
+
+      axios({
+        url: drf.article.comment(articlePk, commentPk),
+        method: "put",
+        data: comment,
+        headers: getters.authHeader,
+      })
+        .then((response) => {
+          commit("SET_ARTICLE_COMMENTS", response.data);
+        })
+        .catch((error) => console.error(error.response));
     },
   },
 };
